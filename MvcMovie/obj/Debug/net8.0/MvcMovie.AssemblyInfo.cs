04080004f215f4df4adbacd7388e780885ff40d1
Phamtuan1234 <phamminhtuanhp0123@gmail.com>
2024-09-17 10:34:36 +0700

    CSS - HTML

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c715a3a2b7febdb086f179272b4adaf56236abde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae5f054630e9fed9545a318cc39d60fe1e482d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
