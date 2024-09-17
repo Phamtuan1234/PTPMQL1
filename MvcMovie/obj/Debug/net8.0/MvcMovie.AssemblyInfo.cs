cddc7f7c53d2dc99f5b9d83e2f2d4e53ccc014bf
Phamtuan1234 <phamminhtuanhp0123@gmail.com>
2024-09-17 15:06:39 +0700

    tạo bảng + sinh code tự động

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae5f054630e9fed9545a318cc39d60fe1e482d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04080004f215f4df4adbacd7388e780885ff40d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
