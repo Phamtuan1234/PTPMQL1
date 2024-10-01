1c8a8d68fa5f9dd7b1759b94119daff30ab09bc4
Phamtuan1234 <phamminhtuanhp0123@gmail.com>
2024-10-01 14:11:31 +0700

    bổ sung

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf5ef51eb9d11993c340c7e103aea4dc742d13c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f9e091ad55c07e0190115708284afc47c1da2e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
